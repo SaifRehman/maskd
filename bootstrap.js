dd6f0108656cd1acff21945f2b219021ace6d2d6
saif <s4saif.121@gmail.com>
2020-06-26 18:30:09 +0400

    added

--- bootstrap.js
+++ bootstrap.js
@@ -22,7 +22,7 @@ const main = async () => {
     var camera = new cv.VideoCapture(0);
     var window = new cv.NamedWindow('Video', 0)
     setInterval(async () => {
-      var = im = await camera.read(function (err, im) {
+      var im = await camera.read(function (err, im) {
         if (err) throw err;
         console.log(im.size())
         if (im.size()[0] > 0 && im.size()[1] > 0) {
