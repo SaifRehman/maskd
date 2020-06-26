de0a60b56c3cfe5b37149382a6e80bdd550fd4be
saif <s4saif.121@gmail.com>
2020-06-26 23:26:54 +0400

    added lens json

--- bootstrap.js
+++ bootstrap.js
@@ -1,42 +1,37 @@
 var cv = require('opencv');
-const util = require('util')
+const { Readable } = require("stream");
 
 const {
   Node,
   generateSecretKey
 } = require("flatend");
-var data ;
-const send = async (ctx) => {
-  ctx.send(`${data}`);
-}
 const main = async () => {
   const node = await Node.start({
     secretKey: generateSecretKey(),
-    bindAddrs: [`:9000`],
-    services: {
-      bootstrap: send
-    }
+    addrs: [`:9000`]
   });
-  try {
-    var camera = await new cv.VideoCapture(0);
-    var window = await new cv.NamedWindow('Video', 0)
-    setInterval(async () => {
-      var image = await camera.read( (err, im) => {
-        if (err) throw err;
-        data = im;
-        console.log(util.inspect(im, false, null, true /* enable colors */))
-
-        if (im.size()[0] > 0 && im.size()[1] > 0) {
-          if (err) throw err;
-          // window.show(im);
-        }
-        // window.blockingWaitKey(0, 50);
+  const camera = new cv.VideoCapture(0);
+  const window = new cv.NamedWindow('Video', 0);
+  setInterval(async () => {
+    try {
+      const image = await new Promise((resolve, reject) => {
+        camera.read((err, image) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(image)
+          }
+        })
       });
-      return image
-    }, 100);
-  } catch (e) {
-    console.log("Couldn't start camera:", e)
-  }
+      const [width, height] = image.size();
+      if (width === 0 || height === 0) return;
+      const frame = image.toBuffer();
+      console.log(frame);
+      await node.push(["node"], {}, Readable.from(frame));
+    } catch (err) {
+      console.error("Failed to read and send camera frame:", err);
+    }
+  }, 100);
 };
 
 main().catch((err) => console.error(err));
--- node.js
+++ node.js
@@ -1,50 +1,31 @@
-const { Node, generateSecretKey } = require("flatend");
-const { Readable } = require("stream");
-const fs = require("fs");
 var cv = require('opencv');
-var window =  new cv.NamedWindow('Video', 0)
+const { Readable } = require("stream");
+
+const {
+  Node,
+  generateSecretKey
+} = require("flatend");
+const send = async(ctx) => await ctx.body();
 
 const main = async () => {
   const node = await Node.start({
     secretKey: generateSecretKey(),
-    addrs: [`:9000`],
-    services: {},
+    bindAddrs: [`:9000`],
+    services: {
+      node: send
+    }
   });
 
-  setInterval(async () => {
+  setInterval( async () => {
     try {
-      const stream = await node.push(["bootstrap"], {}, Readable.from([]));
-
+      const stream = await node.push(["node"], {}, Readable.from([]));
       for await (const data of stream.body) {
-        try {
-          window.show(`${data}`);
-        } catch (err) {
-
-        }
-        // try {
-          // window.show(`${data}`);
-          // window.blockingWaitKey(0, 50);
-          // setInterval(async () => {
-          //   var image = await camera.read( (err, im) => {
-          //     if (err) throw err;
-          //     data = im;
-          //     console.log(im.size())
-          //     if (im.size()[0] > 0 && im.size()[1] > 0) {
-          //       if (err) throw err;
-          //       window.show(im);
-          //     }
-          //     window.blockingWaitKey(0, 50);
-          //   });
-          //   return image
-          // }, 100);
-        // } catch (e) {
-        //   console.log("Couldn't start camera:", e)
-        // }
         console.log(
-          `GOT ${data} byte(s) from service ["bootstrap"].`
+          `GOT ${data} byte(s) from service ["node"].`
         );
       }
     } catch (err) {
+      console.log('error')
       console.error(err.message);
     }
   }, 100);
