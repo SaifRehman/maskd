29a8ee54352938dd93bf51b87a121bb80fe0c077
saif <s4saif.121@gmail.com>
2020-06-26 20:57:31 +0400

    added

--- bootstrap.js
+++ bootstrap.js
@@ -18,16 +18,15 @@ const main = async () => {
       bootstrap: send
     }
   });
-  const handler = async (im) => {
+  const handler = async (image) => {
     console.log('immm iss', im)
-    const stream = await node.push(["bootstrap"], {}, Readable.from(im));
+    const stream = await node.push(["bootstrap"], {}, Readable.from(image));
     return im;
   }
   try {
     var camera = await new cv.VideoCapture(0);
     var window = await new cv.NamedWindow('Video', 0)
     setInterval(async () => {
-      // const example = async () => {
       var image = await camera.read( (err, im) => {
         if (err) throw err;
         console.log(im.size())
@@ -38,7 +37,6 @@ const main = async () => {
             handler(im).then(data => {
               console.log(data);
             }, error => {
-              // console.log('we have an error', err)
             })
           } catch(err){
             console.log('fuci', err)
@@ -47,13 +45,6 @@ const main = async () => {
         window.blockingWaitKey(0, 50);
       });
       return image
-    // }
-    // example().then(img => {
-    //   console.log('img iss',img);
-      
-    // }, error => {
-    //   console.log('error')
-    // })
     }, 100);
   } catch (e) {
     console.log("Couldn't start camera:", e)
